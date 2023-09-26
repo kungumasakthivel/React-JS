817808885c3062abbe169bd94acdb94cc2e5fed3
DShaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-09-26 20:35:06 +0530

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,28 +2,25 @@ import HomePage from "./components/HomePage";
 import ContactPage from "./components/ContactPage";
 import ProjectPage from "./components/ProjectPage";
 import AboutPage from "./components/AboutPage";
-import {Switch, Route, BrowserRouter} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 import Header from "./components/HomePage/Header";
-import { Component } from "react";
+import React from "react";
 
 
-class App extends Component() {
-  render() {
-  return (
+
+function App() {
+     return (
     <>
     <Header/>
-    <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path="/about" component={AboutPage}/>
-      <Route exact path="/contact" component={ContactPage}/>
-      <Route exact path="project" component={ProjectPage}/>
+      <Route path='/'><HomePage/></Route>
+      <Route exact path="/about" ><AboutPage/></Route>
+      <Route exact path="/contact" ><ContactPage/></Route>
+      <Route exact path="/project" ><ProjectPage/></Route>
     </Switch>
-    </BrowserRouter>
   
     </>
   );
-  }
 }
 
 export default App;
